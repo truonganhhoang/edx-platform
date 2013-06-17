b2f08dadd249219871ab7b6d120c96156451750b
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-06-17 16:49:49 -0400

    Moved slater/search commit to review branch for rbcommons test

---
+++ common/djangoapps/search/es_requests.py
@@ -0,0 +1,254 @@
+import requests
+import json
+import os
+import re
+from pymongo import MongoClient
+
+
+class MongoIndexer:
+
+    def __init__(self, host='localhost', port=27017, content_database='xcontent', file_collection="fs.files",
+                 chunk_collection="fs.chunks", module_database='xmodule', module_collection='modulestore'):
+        self.host = host
+        self.port = port
+        self.client = MongoClient(host, port)
+        self.content_db = self.client[content_database]
+        self.module_db = self.client[module_database]
+        try:
+            self.content_db.collection_names().index(file_collection)
+        except ValueError:
+            print "No collection named: " + file_collection
+            raise
+        try:
+            self.content_db.collection_names().index(chunk_collection)
+        except ValueError:
+            print "No collection named: " + chunk_collection
+        try:
+            self.module_db.collection_names().index(module_collection)
+        except ValueError:
+            print "No collection named: " + module_collection
+        self.file_collection = self.content_db[file_collection]
+        self.chunk_collection = self.content_db[chunk_collection]
+        self.module_collection = self.module_db[module_collection]
+
+    def find_transcripts(self, file_ending=".srt.sjson"):
+        """Returns a cursor that will lazily generate transcript objects when next() is called"""
+        return self.file_collection.find({"filename": re.compile(".*?"+re.escape(file_ending))})
+
+    def find_transcript_content(self, mongo_element):
+        """Finds the corresponding chunk to the file element from a cursor similar to that from find_transcripts"""
+        filename = mongo_element["_id"]["name"]
+        database_object = self.chunk_collection.find_one({"files_id.name": filename})
+        return json.loads(database_object["data"].decode())["text"]
+
+    def module_for_uuid(self, transcript_uuid):
+        """Given the transcript uuid found from the xcontent database, returns the mongo document for the video"""
+        regex_pattern = re.compile(".*?"+str(transcript_uuid)+".*?")
+        video_module = self.module_collection.find_one({"definition.data": regex_pattern})
+        return video_module
+
+    def uuid_from_file_name(self, file_name):
+        """Returns a youtube uuid given the filename of a transcript"""
+        print file_name
+        if file_name[:5] == "subs_":
+            file_name = file_name[5:]
+        return file_name[:file_name.find(".")]
+
+    def index_all_transcripts(self, es_instance, index):
+        cursor = self.find_transcripts()
+        for i in range(0, cursor.count()):
+            check = cursor.next()
+            print check.keys()
+            course = check["_id"]["course"]
+            org = check["_id"]["org"]
+            uuid = self.uuid_from_file_name(check["_id"]["name"])
+            video_module = self.module_for_uuid(uuid)
+            try:
+                display_name = org + " " + course + " " + video_module["metadata"]["display_name"]
+            except TypeError:
+                print "Could not find module for: " + uuid
+                display_name = org + " " + course
+            transcript = " ".join(self.find_transcript_content(check))
+            data = {"course": course, "org": org, "uuid": uuid, "searchable_text": transcript,
+                    "display_name": display_name}
+            type_ = course.replace(".", "-")
+            print es_instance.index_data(index, type_, data)._content
+
+
+class ElasticDatabase:
+
+    def __init__(self, url, index_settings_file, *args):
+        """
+        Will initialize elastic search object with any indices specified by args
+        specifically the url should be something of the form `http://localhost:9200`
+        importantly do not include a slash at the end of the url name.
+
+        args should be a list of dictionaries, each dictionary specifying a JSON mapping
+        to be used for a specific type. See settings.json for a more in-depth example
+
+        Example Dictionary:
+            {"index": "transcript", "type": "6-002x", "mapping":
+                {
+                "properties" : {
+                    "searchable_text": {
+                        "type": "string",
+                        "store": "yes",
+                        "index": "analyzed"
+                       }
+                    }
+                }
+            }
+
+        Eventually we will support different configuration files for different indices, but
+        since this is only indexing transcripts right now it seems excessive"""
+
+        self.url = url
+        self.args = args
+        self.index_settings = json.loads(open(index_settings_file, 'rb').read())
+
+    def parse_args(self):
+        for mapping in self.args:
+            try:
+                json_mapping = json.loads(mapping)
+            except ValueError:
+                print "Badly formed JSON args, please check your mappings file"
+                break
+
+            try:
+                index = json_mapping['index']
+                type_ = json_mapping['type']
+                mapping = json_mapping['mapping']
+                self.setup_index(index)
+                self.setup_type(index, type_, mapping)
+            except KeyError:
+                print "Could not find needed keys. Keys found: "
+                print mapping.keys()
+                continue
+
+    def setup_type(self, index, type_, json_mapping):
+        """
+        json_mapping should be a dictionary starting at the properties level of a mapping.
+
+        The type level will be added, so if you include it things will break. The purpose of this
+        is to encourage loose coupling between types and mappings for better code
+        """
+
+        full_url = "/".join([self.url, index, type_, "_mapping"]) + "/"
+        json_put_body = json.dumps({type_: json_mapping})
+        return requests.put(full_url, data=json_put_body)
+
+    def has_index(self, index):
+        """Checks to see if a given index exists in the database returns existance boolean,
+
+        If this returns something other than a 200 or a 404 something is wrong and so we error"""
+        full_url = "/".join([self.url, index])
+        status = requests.head(full_url).status_code
+        if status == 200:
+            return True
+        if status == 404:
+            return False
+        else:
+            print "Got an unexpected reponse code: " + str(status)
+            raise
+
+    def os_walk_transcript(self, walk_results):
+        """Takes the results of os.walk on the data directory and returns a list of absolute paths"""
+        file_check = lambda walk: len(walk[2]) > 0
+        srt_prelim = lambda walk: ".srt.sjson" in " ".join(walk[2])
+        relevant_results = (entry for entry in walk_results if file_check(entry) and srt_prelim(entry))
+        return (self.os_path_tuple_srts(result) for result in relevant_results)
+
+    def os_path_tuple_srts(self, os_walk_tuple):
+        """Given the path tuples from the os.walk method, constructs absolute paths to transcripts"""
+        srt_check = lambda file_name: file_name[-10:] == ".srt.sjson"
+        directory, subfolders, file_paths = os_walk_tuple
+        return [os.path.join(directory, file_path) for file_path in file_paths if srt_check(file_path)]
+
+    def index_directory_transcripts(self, directory, index, type_):
+        """Indexes all transcripts that are present in a given directory
+
+        Will recursively go through the directory and assume all .srt.sjson files are transcript"""
+        # Needs to be lazily evaluatedy
+        transcripts = self.os_walk_transcript(os.walk(directory))
+        for transcript_list in transcripts:
+            for transcript in transcript_list:
+                print self.index_transcript(index, type_, transcript)
+
+    def index_transcript(self, index, type_, transcript_file):
+        """opens and indexes the given transcript file as the given index, type, and id"""
+        file_uuid = transcript_file.rsplit("/")[-1][:-10]
+        transcript = open(transcript_file, 'rb').read()
+        searchable_text = " ".join(filter(None, json.loads(transcript)["text"])).replace("\n", " ")
+        data = {"searchable_text": searchable_text, "uuid": file_uuid}
+        return self.index_data(index, type_, data)._content
+
+    def setup_index(self, index):
+        """Creates a new elasticsearch index, returns the response it gets"""
+        full_url = "/".join([self.url, index]) + "/"
+        return requests.post(full_url, data=json.dumps(self.index_settings))
+
+    def add_index_settings(self, index, index_settings=None):
+        """Allows the editing of an index's settings"""
+        index_settings = index_settings or self.index_settings
+        full_url = "/".join([self.url, index]) + "/"
+        #closing the index so it can be changed
+        requests.post(full_url+"/_close")
+        response = requests.post(full_url+"/", data=json.dumps(index_settings))
+        #reopening the index so it can be read
+        requests.post(full_url+"/_open")
+        return response
+
+    def index_data(self, index, type_, data):
+        """Data should be passed in as a dictionary, assumes it matches the given mapping"""
+        full_url = "/".join([self.url, index, type_]) + "/"
+        response = requests.post(full_url, json.dumps(data))
+        return response
+
+    def get_index_settings(self, index):
+        """Returns the current settings of a given index"""
+        full_url = "/".join([self.url, index, "_settings"])
+        return json.loads(requests.get(full_url)._content)
+
+    def delete_index(self, index):
+        full_url = "/".join([self.url, index])
+        return requests.delete(full_url)
+
+    def get_type_mapping(self, index, type_):
+        """Return the current mapping of the indicated type"""
+        full_url = "/".join([self.url, index, type_, "_mapping"])
+        return json.loads(requests.get(full_url)._content)
+
+    def generate_dictionary(self, index, type_, output_file):
+        """Generates a suitable pyenchant dictionary based on the current state of the database"""
+        base_url = "/".join([self.url, index, type_])
+        words = set()
+        id_ = 1
+        status_code = 200
+        while status_code == 200:
+            transcript = requests.get(base_url+"/"+str(id_))
+            status_code = transcript.status_code
+            try:
+                text = json.loads(transcript._content)["_source"]["searchable_text"]
+                words |= set(re.findall(r'[a-z]+', text.lower()))
+            except KeyError:
+                pass
+            id_ += 1
+            print id_
+        with open(output_file, 'wb') as dictionary:
+            for word in words:
+                dictionary.write(word + "\n")
+
+url = "https://localhost:9200"
+settings_file = "settings.json"
+
+mongo = MongoIndexer()
+test = ElasticDatabase("http://localhost:9200", settings_file)
+#print test.delete_index("transcript-index")
+#print test.add_index_settings("transcript-index")._content
+#print test.get_index_settings("transcript-index")
+mongo.index_all_transcripts(test, "transcript-index")
+
+#print test.setup_type("transcript", "cleaning", mapping)._content
+#print test.get_type_mapping("transcript-index", "2-1x")
+#print test.index_directory_transcripts("/home/slater/edx_all/data", "transcript-index", "transcript")
+# test.generate_dictionary("transcript-index", "transcript", "pyenchant_corpus.txt")
---
+++ common/djangoapps/search/views.py
@@ -0,0 +1,134 @@
+from django.http import HttpResponse, Http404
+from django.core.context_processors import csrf
+from mitxmako.shortcuts import render_to_string
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+import requests
+import json
+import nltk.data
+import nltk.corpus as word_filter
+import enchant
+import string
+
+
+def search(request):
+    context = {}
+    results_string = ""
+    if request.GET:
+        results_string = find(request)
+        context.update({"old_query": request.GET['s']})
+    search_bar = render_to_string("search.html", context)
+    return HttpResponse(search_bar + results_string)
+
+
+def find(request, database="http://127.0.0.1:9200",
+         field="searchable_text", max_result=100):
+    query = request.GET.get("s", "")
+    page = request.GET.get("page", 1)
+    results_per_page = request.GET.get("results", 15)
+    ordering = request.GET.get("ordering", False)
+    index = request.GET.get("content", "transcript")+"-index"
+    full_url = "/".join([database, index, "_search?q="+field+":"])
+    context = {}
+
+    try:
+        results = json.loads(requests.get(full_url+query+"&size="+str(max_result))._content)["hits"]["hits"]
+        data = [entry["_source"] for entry in results]
+        #titles = [entry["display_name"] for entry in data]
+        uuids = [entry["display_name"] for entry in data]
+        transcripts = [entry["searchable_text"] for entry in data]
+        snippets = [snippet_generator(transcript, query) for transcript in transcripts]
+        data = zip(uuids, snippets)
+        data = proper_page(page, data, results_per_page)
+    except KeyError:
+        data = [("No results found", "Please try again")]
+    context.update({"data": data})
+
+    correction = spell_check(query)
+    results_pages = Paginator(data, results_per_page)
+    context.update({"spelling_correction": correction})
+    return render_to_string("search_templates/results.html", context)
+
+
+def query_reduction(query, stopwords):
+    return [word.lower() for word in query.split() if word not in stopwords]
+
+
+def proper_page(page, data, results_per_page=15):
+    pages = Paginator(data, results_per_page)
+    correct_page = ""
+    try:
+        correct_page = pages.page(page)
+    except PageNotAnInteger:
+        correct_page = pages.page(1)
+    except EmptyPage:
+        correct_page = pages.page(pages.num_pages)
+    return correct_page
+
+
+def match(words):
+    contained = lambda words: (words[0] in words[1]) or (words[1] in words[0])
+    near_size = lambda words: abs(len(words[0]) - len(words[1])) < (len(words[0])+len(words[1]))/6
+    return contained(words) and near_size(words)
+
+
+def match_highlighter(query, response, tag="b", css_class="highlight", highlight_stopwords=True):
+    wrapping = ("<"+tag+" class="+css_class+">", "</"+tag+">")
+    punctuation_map = dict((ord(char), None) for char in string.punctuation)
+    depunctuation = lambda word: word.translate(punctuation_map)
+    wrap = lambda text: wrapping[0] + text + wrapping[1]
+    stop_words = word_filter.stopwords.words("english") * (not highlight_stopwords)
+    query_set = set(query_reduction(query, stop_words))
+    bold_response = ""
+    for word in response.split():
+        if any(match((query_word, depunctuation(word.lower()))) for query_word in query_set):
+            bold_response += wrap(word) + " "
+        else:
+            bold_response += word + " "
+    return bold_response
+
+
+def snippet_generator(transcript, query, soft_max=50, word_margin=30, bold=True):
+    punkt = nltk.data.load('tokenizers/punkt/english.pickle')
+    stop_words = word_filter.stopwords.words("english")
+    sentences = punkt.tokenize(transcript)
+    substrings = query_reduction(query, stop_words)
+    query_container = lambda sentence: any(substring in sentence.lower() for substring in substrings)
+    tripped = False
+    response = ""
+    for sentence in sentences:
+        if not tripped:
+            if query_container(sentence):
+                tripped = True
+                response += sentence
+        else:
+            if (len(response.split()) + len(sentence.split()) < soft_max):
+                response += " " + sentence
+            else:
+                response += " " + " ".join(sentence.split()[:word_margin])
+                break
+    # If this is a phonetic match, there might not be a direct text match
+    if tripped is False:
+        for sentence in sentences:
+            if (len(response.split())+len(sentence.split())) < soft_max:
+                response += " " + sentence
+            else:
+                response += " " + " ".join(sentence.split()[:word_margin])
+                break
+    if bold:
+        response = match_highlighter(query, response)
+    return response
+
+
+def spell_check(query, pyenchant_dictionary_file="common/djangoapps/search/pyenchant_corpus.txt", stopwords=set()):
+    """Returns corrected version with attached html if there are suggested corrections."""
+    dictionary = enchant.request_pwl_dict(pyenchant_dictionary_file)
+    words = query_reduction(query, stopwords)
+    try:
+        possible_corrections = [dictionary.suggest(word)[0] for word in words]
+    except IndexError:
+        return False
+    if possible_corrections == words:
+        return None
+    else:
+        return " ".join(possible_corrections)

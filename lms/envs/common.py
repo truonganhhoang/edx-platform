1241198a2b8b4c2d6c8ab02840c71628cb8470d4
Miles Steele <msteele@edx.org>
2013-07-01 13:26:53 -0400

    add feature switch for beta dashboard

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -141,6 +141,9 @@ MITX_FEATURES = {
 
     # Enable instructor dash to submit background tasks
     'ENABLE_INSTRUCTOR_BACKGROUND_TASKS': True,
+
+    # Enable instructor dash to submit background tasks
+    'ENABLE_INSTRUCTOR_BETA_DASHBOARD': False,
 }
 
 # Used for A/B testing
